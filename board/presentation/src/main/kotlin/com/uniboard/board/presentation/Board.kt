b080c914d8694f28116bea31aeba2da1a90085b7
renattele <renattele@yandex.com>
2024-03-27 14:13:50 +0300

    Added logging to Board

--- board/presentation/src/main/kotlin/com/uniboard/board/presentation/Board.kt
+++ board/presentation/src/main/kotlin/com/uniboard/board/presentation/Board.kt
@@ -5,7 +5,7 @@ import com.uniboard.board.domain.BoardObject
 import com.uniboard.board.domain.BoardRepository
 import com.uniboard.board.presentation.socket.dsl.*
 import com.uniboard.board.presentation.socket.sockets
-import com.uniboard.core.domain.BuildConfig
+import com.uniboard.core.domain.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
@@ -17,10 +17,22 @@ import kotlinx.serialization.json.*
 import org.koin.core.context.GlobalContext
 import org.koin.ktor.ext.inject
 
+private object Tags {
+    const val BOARD_SOCKETS = "Board Sockets"
+    const val BOARD_SOCKETS_CONNECTED = "$BOARD_SOCKETS Connected"
+    const val BOARD_SOCKETS_CREATED = "$BOARD_SOCKETS Created"
+    const val BOARD_SOCKETS_MODIFIED = "$BOARD_SOCKETS Modified"
+    const val BOARD_SOCKETS_DELETED = "$BOARD_SOCKETS Deleted"
+    const val BOARD_API = "Board API"
+    const val BOARD_API_CREATEDBOARD = "$BOARD_API CreateBoard"
+    const val BOARD_API_ALL_OBJECTS = "$BOARD_API All objects"
+}
+
 @KtorDsl
 fun Application.board() {
     val allboards by inject<AllBoardsRepository>()
     val boardRepository by inject<BoardRepository>()
+    val logger by inject<Logger>()
     val buildConfig by inject<BuildConfig>()
     if (buildConfig.SOCKETS_ENABLED) {
         val coroutineScope by inject<CoroutineScope>()
@@ -28,51 +40,102 @@ fun Application.board() {
     }
     routing {
         post("/createboard") {
-            val newID = allboards.add()
-            call.respond(mapOf("id" to newID))
+            logger.withTag(Tags.BOARD_API_CREATEDBOARD) {
+                info("Creating new board")
+                val newID = allboards.add()
+                info("New board ID: $newID")
+                call.respond(mapOf("id" to newID))
+                info("Send response")
+            }
         }
         get("/board/{id}/get") {
-            val id = call.parameters["id"]?.toLongOrNull()
-            if (id == null || !allboards.exists(id)) {
-                call.respond(HttpStatusCode.BadRequest)
-                return@get
+            logger.withTag(Tags.BOARD_API_ALL_OBJECTS) {
+                info("Getting all objects")
+
+                val id = call.parameters["id"]?.toLongOrNull()
+                info("Requested ID: $id")
+
+                if (id == null || !allboards.exists(id)) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    warn("Bad Request, ID: $id")
+
+                    if (id != null) {
+                       warn("Board exists: ${allboards.exists(id)}")
+                    }
+                    return@get
+                }
+                val elements = boardRepository.all(id)
+                info("All elements size: ${elements.size}")
+
+                call.respond(elements.map { it.state }.toString().decoded)
+                info("Got all objects")
             }
-            val elements = boardRepository.all(id)
-            call.respond(elements.map { it.state }.toString().decoded)
         }
     }
 }
 
 private fun CoroutineScope.boardSocketServer() = sockets {
-    exception {
-        it.printStackTrace()
-    }
     val boardRepository by inject<BoardRepository>()
-    listen("connected") { data ->
-        val boardId = data.toLongOrNull() ?: sendAndFinish("error", "Board ID is incorrect")
-        join(boardId.toString())
+    val logger by inject<Logger>()
+    exception {
+        logger.error(Tags.BOARD_SOCKETS, it)
+    }
+    logger.withTag(Tags.BOARD_SOCKETS_CONNECTED) {
+        listen("connected") { data ->
+            info("Connecting with $data")
+
+            val boardId = data.toLongOrNull() ?: sendAndFinish("error", "Board ID is incorrect")
+            join(boardId.toString())
+            info("Connected with BoardID=$boardId")
+        }
     }
     listen("created") { data ->
-        val room = requireRoomNotNull()
-        boardRepository.add(room.boardId, BoardObject(data.decoded.jsonObject.id, data))
-        room.send("created", data)
+        logger.withTag(Tags.BOARD_SOCKETS_CREATED + currentRoom?.id) {
+            info("Creating object")
+
+            val room = requireRoomNotNull()
+
+            val element = BoardObject(data.decoded.jsonObject.id, data)
+            boardRepository.add(room.boardId, element)
+            room.send("created", data)
+            info("Created object $element")
+        }
     }
     listen("modified") { data ->
-        val room = requireRoomNotNull()
-        val newData = data.decoded.jsonObject.toMutableMap()
-        val id = newData.id
-        newData.remove("uniboardData")
-        val oldData = boardRepository.get(room.boardId, id).decoded.jsonObject.toMutableMap()
-        newData.forEach { (key, value) ->
-            oldData[key] = value
+        logger.withTag(Tags.BOARD_SOCKETS_MODIFIED + currentRoom?.id) {
+            info("Modifying object $data")
+
+            val room = requireRoomNotNull()
+            val newData = data.decoded.jsonObject.toMutableMap()
+            info("New data is $newData")
+
+            val id = newData.id
+            info("New data ID: $id")
+
+            newData.remove("uniboardData")
+            val oldData = boardRepository.get(room.boardId, id).decoded.jsonObject.toMutableMap()
+            info("Old data: $oldData")
+
+            newData.forEach { (key, value) ->
+                oldData[key] = value
+            }
+            info("Modified data is $oldData")
+
+            val element = BoardObject(id, JsonObject(oldData).encoded)
+            boardRepository.set(room.boardId, element)
+            room.send("modified", data)
+            info("Modified object $element")
         }
-        boardRepository.set(room.boardId, BoardObject(id, JsonObject(oldData).encoded))
-        room.send("modified", data)
     }
-    listen("deleted") { data ->
-        val room = requireRoomNotNull()
-        boardRepository.delete(room.boardId, data)
-        room.send("deleted", data)
+    listen("deleted") { id ->
+        logger.withTag(Tags.BOARD_SOCKETS_DELETED + currentRoom?.id) {
+            info("Deleting object with ID: $id")
+
+            val room = requireRoomNotNull()
+            boardRepository.delete(room.boardId, id)
+            room.send("deleted", id)
+            info("Deleted object with ID: $id")
+        }
     }
 }
 
--- core/domain/src/main/kotlin/com/uniboard/core/domain/Logger.kt
+++ core/domain/src/main/kotlin/com/uniboard/core/domain/Logger.kt
@@ -15,3 +15,25 @@ fun Logger.warn(tag: String, message: String) =
 
 fun Logger.error(tag: String, message: String) =
     log(LogLevel.Error, tag, message)
+
+fun Logger.error(tag: String, throwable: Throwable) =
+    error(tag, throwable.message ?: "")
+
+inline fun Logger.withTag(tag: String, block: context(String, Logger) () -> Unit) {
+    block(tag, this)
+}
+
+context(String)
+fun Logger.info(message: String) = info(this@String, message)
+
+context(String)
+fun Logger.debug(message: String) = debug(this@String, message)
+
+context(String)
+fun Logger.warn(message: String) = warn(this@String, message)
+
+context(String)
+fun Logger.error(message: String) = error(this@String, message)
+
+context(String)
+fun Logger.error(throwable: Throwable) = error(this@String, throwable)
