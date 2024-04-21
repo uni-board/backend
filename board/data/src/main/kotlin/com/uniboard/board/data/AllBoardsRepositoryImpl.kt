64476ec0a5163a61fda41763e2241c290bd3a014
renattele <renattele@yandex.com>
2024-04-21 17:26:39 +0300

    Updated AllBoardsRepositoryImpl with edit and settings

--- board/data/src/main/kotlin/com/uniboard/board/data/AllBoardsRepositoryImpl.kt
+++ board/data/src/main/kotlin/com/uniboard/board/data/AllBoardsRepositoryImpl.kt
@@ -3,48 +3,59 @@ package com.uniboard.board.data
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoCollection
 import com.mongodb.client.MongoDatabase
+import com.mongodb.client.model.UpdateOptions
+import com.mongodb.client.model.Updates
 import com.uniboard.board.domain.AllBoardsRepository
+import org.bson.Document
 import java.util.*
 
 class AllBoardsRepositoryImpl(private val mongoClient: MongoClient) : AllBoardsRepository {
     private val nameDatabase = "CreatedBoards"
+    private val query = Document("isSettings", true)
 
-    private val freeId: UUID
-        get() = UUID.randomUUID()
+    private fun hasCollection(db: MongoDatabase, collectionName: String?): Boolean {
+        assert(!collectionName.isNullOrEmpty())
+        db.listCollectionNames().iterator().use { cursor ->
+            while (cursor.hasNext()) if (cursor.next() == collectionName) return true
+        }
+        return false
+    }
+
+    private fun getFreeId(): UUID {
+        return UUID.randomUUID()
+    }
 
 
     override fun add(): String {
-        val database = mongoClient.getDatabase(nameDatabase)
-        val uuid = freeId
+        val database: MongoDatabase = mongoClient.getDatabase(nameDatabase)
+        val uuid = getFreeId()
         database.createCollection(uuid.toString())
         return uuid.toString()
     }
 
     override fun delete(id: String) {
-        val database = mongoClient.getDatabase("CreatedBoards")
+        val database: MongoDatabase = mongoClient.getDatabase("CreatedBoards")
         val todoCollection: MongoCollection<*> = database.getCollection(id)
         todoCollection.drop()
     }
 
     override fun exists(id: String): Boolean {
-        val database = mongoClient.getDatabase("CreatedBoards")
+        val database: MongoDatabase = mongoClient.getDatabase("CreatedBoards")
         return hasCollection(database, id)
     }
 
     override fun settings(id: String): String {
-        return "{}"
+        val database: MongoDatabase = mongoClient.getDatabase("CreatedBoards")
+        val todoCollection: MongoCollection<*> = database.getCollection(id)
+        val h = todoCollection.find(Document("isSettings", true)).first() as Document
+        return h["settings"].toString()
     }
 
     override fun edit(id: String, settings: String) {
-        // TODO
-    }
-
-    companion object {
-        private fun hasCollection(db: MongoDatabase, collectionName: String?): Boolean {
-            db.listCollectionNames().iterator().use { cursor ->
-                while (cursor.hasNext()) if (cursor.next() == collectionName) return true
-            }
-            return false
-        }
+        val database: MongoDatabase = mongoClient.getDatabase("CreatedBoards")
+        val todoCollection: MongoCollection<*> = database.getCollection(id)
+        val updates = Updates.set("settings", settings)
+        val options = UpdateOptions().upsert(true)
+        todoCollection.updateOne(query, updates, options)
     }
 }
